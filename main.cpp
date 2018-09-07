b6a81570f77fca60d28eb235f8fd467213085425
axelzucho <axelzucho1@gmail.com>
2018-09-07 14:03:48 -0500

    Separating the reading and displaying in two separate functions

--- main.cpp
+++ main.cpp
@@ -6,22 +6,31 @@
 
 using namespace cv;
 using namespace std;
-int main( int argc, char** argv )
-{
-    string imageName("/home/axel/Downloads/image.jpeg"); // by default
-    if( argc > 1)
-    {
-        imageName = argv[1];
-    }
-    Mat image;
-    image = imread(imageName.c_str(), IMREAD_COLOR); // Read the file
-    if( image.empty() )                      // Check for invalid input
-    {
-        cout <<  "Could not open or find the image" << std::endl ;
-        return -1;
-    }
+
+void show_image(Mat image){
     namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
     imshow( "Display window", image );                // Show our image inside it.
     waitKey(0); // Wait for a keystroke in the window
+}
+
+void read_image(int argc, char** argv, string image_name){
+    if( argc > 1)
+    {
+        image_name = argv[1];
+    }
+    Mat image;
+    image = imread(image_name.c_str(), IMREAD_COLOR); // Read the file
+    if( image.empty() )                      // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return;
+    }
+    show_image(image);
+}
+
+
+int main( int argc, char** argv )
+{
+    read_image(argc, argv, "/home/axel/Documents/Proyecto_Reconocimiento/facial_transformation/images/Cara1.jpeg"); // by default
     return 0;
 }
