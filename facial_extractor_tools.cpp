6b8fd0e1a0cb10732b124e16288c0bbd3b0e50ef
axelzucho <axelzucho1@gmail.com>
2018-09-17 12:39:42 -0500

    Fixing warnings and cleaning up example

--- facial_extractor_tools.cpp
+++ facial_extractor_tools.cpp
@@ -19,6 +19,7 @@ using namespace std;
 using cv::Mat;
 using cv::Scalar;
 using cv::Rect;
+using cv::Size;
 using cv::WINDOW_AUTOSIZE;
 using cv::namedWindow;
 using cv::waitKey;
@@ -39,28 +40,28 @@ using dlib::cv_image;
 
 namespace extractor {
 
-    static point get_average(point left, point right) {
+    static point get_average(const point &left, const point &right) {
         point average((left.x() + right.x()) / 2, (left.y() + right.y()) / 2);
         return average;
     }
 
-    void show_image(Mat image) {
+    void show_image(const Mat &image) {
         namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
         imshow("Display window", image);                // Show our image inside it.
         waitKey(0); // Wait for a keystroke in the window
     }
 
-    void show_image_with_points(dlib::full_object_detection shape, rectangle face_rectangle, Mat image, int size_x,
-                                int size_y) {
-        std::vector <image_window::overlay_circle> points;
+    void show_image_with_points(const dlib::full_object_detection &shape, const rectangle &face_rectangle,
+                                const Mat &image) {
+        std::vector<image_window::overlay_circle> points;
         image_window win;
-        win.set_size(size_x, size_y);
+        win.set_size(image.cols, image.rows);
         win.set_title("Detector");
 
         while (!win.is_closed()) {
             for (unsigned int n = 0; n < shape.num_parts(); n++) {
-                point pt = shape.part(n);
-                points.push_back(image_window::overlay_circle(pt, 2, dlib::rgb_pixel(255, 255, 0)));
+                const point &pt = shape.part(n);
+                points.emplace_back(image_window::overlay_circle(pt, 2, dlib::rgb_pixel(255, 255, 0)));
             }
 
             win.clear_overlay();
@@ -70,15 +71,15 @@ namespace extractor {
         }
     }
 
-    void face_landmark_detector(string path_to_model, rectangle face_rectangle, Mat mat_image,
+    void face_landmark_detector(const string &path_to_model, const rectangle &face_rectangle, const Mat &mat_image,
                                 full_object_detection *shape) {
         dlib::shape_predictor sp;
         dlib::deserialize(path_to_model) >> sp;
-        dlib::array2d <rgb_pixel> dlib_image;
+        dlib::array2d<rgb_pixel> dlib_image;
         assign_image(dlib_image, cv_image<rgb_pixel>(mat_image));
 
         *shape = sp(dlib_image, face_rectangle);
-        show_image_with_points(*shape, face_rectangle, mat_image, 960, 1280);
+        show_image_with_points(*shape, face_rectangle, mat_image);
     }
 
     void read_image(int argc, char **argv, string image_path, Mat *image) {
@@ -92,43 +93,43 @@ namespace extractor {
         }
     }
 
-    void align_image(double size_x, double size_y, point left_eye_before, point right_eye_before, Mat image,
-                     double left_eye_after_x, double left_eye_after_y, Mat *template_image) {
-        int dx = right_eye_before.x() - left_eye_before.x();
-        int dy = right_eye_before.y() - left_eye_before.y();
+    void align_image(const Size &size, const point &left_eye_before, const point &right_eye_before, const Mat &image,
+                     const double &left_eye_after_x, const double &left_eye_after_y, Mat *template_image) {
+        long dx = right_eye_before.x() - left_eye_before.x();
+        long dy = right_eye_before.y() - left_eye_before.y();
         double angle = atan2(dy, dx) * 180 / PI;
 
         double right_eye_after_x = 1 - left_eye_after_x;
         double distance_before = sqrt(dx * dx + dy * dy);
         double desired_distance = right_eye_after_x - left_eye_after_x;
-        desired_distance *= size_x;
+        desired_distance *= size.width;
         double scale = desired_distance / distance_before;
 
         point center_between_eyes = get_average(left_eye_before, right_eye_before);
-        cv::Point2f center_between_eyes_f((float) center_between_eyes.x(), (float) center_between_eyes.y());
+        cv::Point2f center_between_eyes_f(center_between_eyes.x(), center_between_eyes.y());
         Mat rotation_matrix = cv::getRotationMatrix2D(center_between_eyes_f, angle, scale);
 
-        double tx = size_x / 2;
-        double ty = size_y * left_eye_after_y;
+        double tx = (float)size.width / 2;
+        double ty = size.height * left_eye_after_y;
         rotation_matrix.at<double>(0, 2) += tx - center_between_eyes.x();
         rotation_matrix.at<double>(1, 2) += ty - center_between_eyes.y();
-        cv::warpAffine(image, *template_image, rotation_matrix, cv::Size(size_x, size_y), cv::INTER_CUBIC);
+        cv::warpAffine(image, *template_image, rotation_matrix, cv::Size(size.width, size.height), cv::INTER_CUBIC);
     }
 
 
-    void example(int argc, char **argv) {
+    void example(int argc, char **argv, const string &path_to_image, const string &path_to_model, const rectangle &face,
+                 const Size &template_size, const float &left_eye_x, const float &left_eye_y) {
         Mat image;
-        read_image(argc, argv, "/home/axelzucho/Documents/Github/facial_transformation/images/Cara1.jpeg",
+        read_image(argc, argv, path_to_image,
                    &image); // by default
         full_object_detection shape;
-        rectangle face(330, 320, 640, 630);
         face_landmark_detector(
-                "/home/axelzucho/Documents/Github/facial_transformation/shape_predictor_68_face_landmarks.dat", face,
+               path_to_model, face,
                 image, &shape);
         Mat template_image;
         point left_eye = get_average(shape.part(37), shape.part(40));
         point right_eye = get_average(shape.part(43), shape.part(46));
-        align_image(500, 500, left_eye, right_eye, image, 0.3, 0.3, &template_image);
+        align_image(template_size, left_eye, right_eye, image, left_eye_x, left_eye_y, &template_image);
         show_image(template_image);
     }
 
--- facial_extractor_tools.h
+++ facial_extractor_tools.h
@@ -4,13 +4,22 @@
 #include "facial_extractor_tools.cpp"
 
 namespace extractor {
-    static point get_average(point left, point right);
-    void show_image(Mat image);
-    void show_image_with_points(full_object_detection shape, rectangle face_rectangle, Mat image, int size_x, int size_y);
-    void face_landmark_detector(string path_to_model, rectangle face_rectangle, Mat mat_image, full_object_detection* shape);
-    void read_image(int argc, char** argv, string image_path, Mat* image);
-    void align_image(double size_x, double size_y, point left_eye_before, point right_eye_before, Mat image, double left_eye_after_x, double left_eye_after_y, Mat* template_image);
-    void example(int argc, char** argv);
+    static point get_average(const point &left, const point &right);
+
+    void show_image(const Mat &image);
+
+    void show_image_with_points(const full_object_detection &shape, const rectangle &face_rectangle, const Mat &image);
+
+    void face_landmark_detector(const string &path_to_model, const rectangle &face_rectangle, const Mat &mat_image,
+                                full_object_detection *shape);
+
+    void read_image(int argc, char **argv, string image_path, Mat *image);
+
+    void align_image(const Size &size, const point &left_eye_before, const point &right_eye_before, const Mat &image,
+                     const double &left_eye_after_x, const double &left_eye_after_y, Mat *template_image);
+
+    void example(int argc, char **argv, const string &path_to_image, const string &path_to_model, const rectangle &face,
+                 const Size &template_size, const float &left_eye_x, const float &left_eye_y);
 } // namespace extractor
 
 #endif
