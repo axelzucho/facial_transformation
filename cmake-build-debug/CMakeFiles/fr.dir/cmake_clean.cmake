eb6c01f8f94d41f09d86f81aa825da523b12c372
axelzucho <axelzucho1@gmail.com>
2018-09-17 11:13:49 -0500

    Refactoring into header and source file

---
+++ cmake-build-debug/CMakeFiles/fr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fr.dir/main.cpp.o"
+  "fr.pdb"
+  "fr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
